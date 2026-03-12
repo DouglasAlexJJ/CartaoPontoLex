8109449c5ba4c3329f2300b939762db84f4f1238
DouglasAlexJJ <douglasalexjj@outlook.com.br>
2026-03-11 22:26:49 -0300

    vamos ver oq dá

--- js/firebase-config.js
+++
@@ -1,9 +0,0 @@
-// Arquivo: js/firebase-config.js
-export const firebaseConfig = {
-    apiKey: "AIzaSyAYKwESZLQelQlyh5pWX0oE0eVOMI5Z3fY", // A sua chave real
-    authDomain: "cartaopontolex.firebaseapp.com",
-    projectId: "cartaopontolex",
-    storageBucket: "cartaopontolex.firebasestorage.app",
-    messagingSenderId: "261448645689",
-    appId: "1:261448645689:web:a6e7aebb12ef87c15b61e8"
-};
