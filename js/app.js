3426380279a2b6d84df83da3bf65f9e3c12bab5a
DouglasAlexJJ <douglasalexjj@outlook.com.br>
2026-03-11 23:38:26 -0300

    Revert "ignorar a chave do firebase"
    This reverts commit 4f30d37ab49fbb9d0ccf6414d913c93f06a25aa0.

--- js/app.js
+++ js/app.js
@@ -5,7 +5,16 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
 import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js";
 import { getFirestore, doc, getDoc, updateDoc } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-firestore.js";
-import { firebaseConfig } from "./firebase-config.js";
+
+// Suas Chaves
+const firebaseConfig = {
+  apiKey: "AIzaSyAYKwESZLQelQlyh5pWX0oE0eVOMI5Z3fY",
+  authDomain: "cartaopontolex.firebaseapp.com",
+  projectId: "cartaopontolex",
+  storageBucket: "cartaopontolex.firebasestorage.app",
+  messagingSenderId: "261448645689",
+  appId: "1:261448645689:web:a6e7aebb12ef87c15b61e8"
+};
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
